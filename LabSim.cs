fa8538c134429aedb75e2cc6b26d8c1bbb1aa62a
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-03 14:39:44 +0200

    asfsdg

--- LabSim.cs
+++ LabSim.cs
@@ -2,8 +2,14 @@
 {
     internal class LabSim
     {
-        private List<string> adatsorok;
+        private List<string> adatsorok = [];
         private char[,] lab;
+
+        public int OszlopokSzama => adatsorok[0].Length;
+        public int SorokSzama => adatsorok.Count;
+
+        public int KijaratOszlopIndex => OszlopokSzama - 1;
+        public int KijaratSorIndex => SorokSzama - 2;
 
         public void AdatsorokBeolvasasa(string forras)
         {
@@ -11,9 +17,70 @@
             while (!sr.EndOfStream) adatsorok.Add(sr.ReadLine());
         }
 
+        public void LabFeltoltese()
+        {
+            for (int s = 0; s < adatsorok.Count; s++)
+            {
+                for (int o = 0; o < adatsorok[s].Length; o++)
+                {
+                    lab[s, o] = adatsorok[s][o];
+                }
+            }
+        }
+
+        public void LabKiir()
+        {
+            for (int s = 0; s < lab.GetLength(0); s++)
+            {
+                for (int o = 0; o < lab.GetLength(1); o++)
+                {
+
+                    if (lab[s, o] == '-') Console.ForegroundColor = ConsoleColor.Red;
+                    if (lab[s, o] == 'O') Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(lab[s, o]);
+                    Console.ResetColor();
+                }
+                Console.Write('\n');
+            }
+        }
+
+        public void Utkereses()
+        {
+            bool keresesKesz = false;
+            bool nincsMegoldas = false;
+
+            int r = 1;
+            int c = 0;
+
+            while (!keresesKesz && !nincsMegoldas)
+            {
+                Console.Clear();
+                lab[r, c] = 'O';
+                if (lab[r, c + 1] == ' ') c++;
+                else if (lab[r + 1, c] == ' ') r++;
+                else
+                {
+                    lab[r, c] = '-';
+                    if (lab[r, c - 1] == 'O') c--;
+                    else r--;
+                }
+                keresesKesz =
+                    r == KijaratSorIndex
+                    && c == KijaratOszlopIndex;
+                if (keresesKesz) lab[r, c] = 'O';
+                nincsMegoldas = r == 1 && c == 0;
+                this.LabKiir();
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine($"\n\n{(nincsMegoldas ? "Nincs megoldás!": "Van megoldás!")}");
+        }
+
         public LabSim(string fileName)
         {
             AdatsorokBeolvasasa(@$"..\..\..\src\{fileName}");
+            lab = new char[SorokSzama, OszlopokSzama];
+            LabFeltoltese();
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World");
+﻿using LabirintusCLI;
+
+LabSim labsim = new("Lab1.txt");
+//Console.WriteLine("5. feladat: labirintus adatai:");
+//Console.WriteLine($"oszlopok szama: {labsim.OszlopokSzama}");
+//Console.WriteLine($"sorok szama: {labsim.SorokSzama}");
+//Console.WriteLine($"kijarat indexe: " +
+//    $"s:{labsim.KijaratSorIndex}, " +
+//    $"o:{labsim.KijaratOszlopIndex}");
+
+labsim.Utkereses();
