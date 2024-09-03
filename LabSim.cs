ce766e1109b1b32ab5bf356e3f5e226bb3c42fb5
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-03 11:20:10 +0200

    Add project files.

---
+++ LabSim.cs
@@ -0,0 +1,20 @@
+﻿namespace LabirintusCLI
+{
+    internal class LabSim
+    {
+        private List<string> adatsorok;
+        private char[,] lab;
+
+        public void AdatsorokBeolvasasa(string forras)
+        {
+            using StreamReader sr = new(forras);
+            while (!sr.EndOfStream) adatsorok.Add(sr.ReadLine());
+        }
+
+        public LabSim(string fileName)
+        {
+            AdatsorokBeolvasasa(@$"..\..\..\src\{fileName}");
+        }
+
+    }
+}
